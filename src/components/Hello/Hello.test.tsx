528e1de9585871c4ece059e2a05ef5f079575d2a
282Haniwa <282haniwa@gmail.com>
2020-06-21 20:30:31 +0900

    Fix test.

--- src/components/Hello/Hello.test.tsx
+++ src/components/Hello/Hello.test.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import { Hello } from './Hello';
 
-test('Hello component test', () => {
-  const hello = shallow(<Hello compiler="TypeScript" framework="React" />);
+describe('Hello.tsx', () => {
+  test('Hello component test', () => {
+    const hello = shallow(<Hello compiler="TypeScript" framework="React" />);
 
-  // Interaction demo
-  expect(hello.text()).toEqual('Hello from TypeScript and React!');
+    // Interaction demo
+    expect(hello.text()).toEqual('Hello from TypeScript and React!');
 
-  // Snapshot demo
-  expect(hello).toMatchSnapshot();
+    // Snapshot demo
+    expect(hello).toMatchSnapshot();
+  });
 });
